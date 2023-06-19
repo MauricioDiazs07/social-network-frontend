48d0367b4c11a4c755dceecb9cefc7b261b34c21
Mauricio Díaz <mauricio.diaz@brita.ai>
2023-06-19 01:08:55 -0600

    adding language support

---
+++ src/i18n/strings.js
@@ -0,0 +1,7 @@
+import LocalizedStrings from 'react-native-localization';
+// import enI18n from './en';
+import enI18n from './es';
+
+export default strings = new LocalizedStrings({
+  es: enI18n,
+});
